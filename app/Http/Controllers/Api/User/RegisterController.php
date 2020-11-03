3a0a04158dee38f7f8a8661d355b4e7bd4333140
Michael van Olst <michaelvanolst@pop-os.localdomain>
2020-11-03 21:44:47 +0100

    importing user resource

--- app/Http/Controllers/Api/User/RegisterController.php
+++ app/Http/Controllers/Api/User/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\User;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class RegisterController extends Controller
 {
