8bd7d0cb73adc133cf1a320fb35912a1a4f3b45c
Michael van Olst <michaelvanolst@pop-os.localdomain>
2020-11-06 13:54:39 +0100

    added subdomain outside auth

--- routes/api.php
+++ routes/api.php
@@ -24,11 +24,11 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::put('recipes/{recipe}/like', [RecipesLikesController::class, 'update']);
     Route::delete('recipes/{recipe}/unlike', [RecipesLikesController::class, 'destroy']);
 
+});
 
-    Route::domain('images.recipes.michaelvanolst.dev')->group(function () {
-        Route::get('test', function () {
-            return 'test';
-        });
+Route::domain('images.recipes.michaelvanolst.dev')->group(function () {
+    Route::get('test', function () {
+        return 'test';
     });
 });
 
